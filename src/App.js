a3f3e454c3c5d89de38e3344cc55bf5927e1c689
Andy Short <automation.andy101@gmail.com>
2023-03-29 22:03:06 +0100

    Added Note and NoteList components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: 'This is my first note!',
+      date: '15/01/2023'
+    },
+    {
+      id: nanoid(),
+      text: 'This is my second note!',
+      date: '15/02/2023'
+    },
+    {
+      id: nanoid(),
+      text: 'This is my third note!',
+      date: '15/03/2023'
+    },
+  ]);
+
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes={notes} />
     </div>
   );
 };
