2009bec8654fa583f20c0dc2179bfc16fdf682a6
Andy Short <automation.andy101@gmail.com>
2023-03-30 06:47:17 +0100

    Added delete and add functionality

--- src/App.js
+++ src/App.js
@@ -20,11 +20,34 @@ function App() {
       text: 'This is my third note!',
       date: '15/03/2023'
     },
+    {
+      id: nanoid(),
+      text: 'This is my fourth note!',
+      date: '15/04/2023'
+    },
   ]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+    };
+    
+    const newNotes = [...notes, newNote];
+
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  }
 
   return (
     <div className="container">
-      <NotesList notes={notes} />
+      <NotesList notes={notes} handleAddNote={addNote} handleDeleteNote={deleteNote} />
     </div>
   );
 };
